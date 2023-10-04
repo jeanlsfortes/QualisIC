4e7129bb7662e46e2e8288fa60942a71444d9c24
jeanlsfortes <jeanlucaseh10@gmail.com>
2023-10-03 22:24:04 -0400

    pagina teste

--- flutter_application_1/lib/data.dart
+++ flutter_application_1/lib/data.dart
@@ -1,45 +1,82 @@
 import 'dart:convert';
 
-class Document {
-  String title = "My Document";
-  DateTime now = DateTime.now();
+class Block {
+  final String type;
+  final String text;
 
-  Map<String, dynamic> getMetadata() {
-    return {
-      "title": title,
-      "modified": now,
-    };
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json.containsKey('type') && json.containsKey('text')) {
+      var type = json['type'] as String;
+      var text = json['text'] as String;
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
   }
 }
 
+class Document {
+  final Map<String, Object?> _json;
+
+  Document({String? title, DateTime? modified}) : _json = {
+        'metadata': {
+          'title': title ?? 'My Document',
+          'modified': modified?.toIso8601String() ?? DateTime.now().toIso8601String(),
+        },
+        'blocks': [
+          {
+            'type': 'h1',
+            'text': 'Chapter 1',
+          },
+          {
+            'type': 'p',
+            'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          },
+          {
+            'type': 'checkbox',
+            'text': 'Learn Dart 3',
+          },
+        ],
+      };
+
+  Map<String, Object?> getMetadata() {
+    if (_json.containsKey('metadata')) {
+      var metadataJson = _json['metadata'];
+      if (metadataJson is Map) {
+        if (metadataJson.containsKey('title') && metadataJson.containsKey('modified')) {
+          var title = metadataJson['title'] as String;
+          var localModified = metadataJson['modified'] as String;
+          return {
+            'title': title,
+            'modified': DateTime.parse(localModified),
+          };
+        }
+      }
+    }
+    throw const FormatException('Unexpected JSON');
+  }
+
+  List<Block> getBlocks() {
+    if (_json.containsKey('blocks') && _json['blocks'] is List) {
+      var blocksJson = _json['blocks'] as List;
+      return <Block>[
+        for (var blockJson in blocksJson) Block.fromJson(blockJson)
+      ];
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
 
 void main() {
-  Document myDocument = Document();
-  Map<String, dynamic> metadata = myDocument.getMetadata();
+  Document myDocument = Document(title: 'Custom Title', modified: DateTime(2023, 5, 27));
+  Map<String, Object?> metadata = myDocument.getMetadata();
   print(metadata);
-}
 
-
-const documentJson = '''
-{
-  "metadata": {
-    "title": "My Document",
-    "modified": "2023-05-10"
-  },
-  "blocks": [
-    {
-      "type": "h1",
-      "text": "Chapter 1"
-    },
-    {
-      "type": "p",
-      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    },
-    {
-      "type": "checkbox",
-      "checked": false,
-      "text": "Learn Dart 3"
-    }
-  ]
+  List<Block> blocks = myDocument.getBlocks();
+  blocks.forEach((block) {
+    print('Block Type: ${block.type}, Text: ${block.text}');
+  });
 }
-''';
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,46 +1,134 @@
-import 'package:flutter/material.dart';
+import 'data.dart'; // Substitua 'document.dart' pelo caminho real do arquivo
 
-import 'data.dart';
+import 'package:flutter/material.dart';
 
 void main() {
  runApp(const DocumentApp());
 }
 
 class DocumentApp extends StatelessWidget {
- const DocumentApp({super.key});
+  const DocumentApp({Key? key}) : super(key: key);
 
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     theme: ThemeData(useMaterial3: true),
-     home: DocumentScreen(
-       document: Document(),
-     ),
-   );
- }
+  // Adicione o novo método personalizado aqui
+  void myCustomMethod() {
+    // O código da sua função personalizada vai aqui
+    // Por exemplo, você pode adicionar a lógica para realizar alguma ação especial.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // O código do método build continua aqui
+    Document document = Document();
+    var metadataRecord = document.getMetadata();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(metadataRecord['title'] as String),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Last modified ${metadataRecord['modified']}',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+        break;
+      case 'p':
+      case 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+        break;
+      default:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+        break;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
+      ),
+    );
+  }
+}
+
+
 class DocumentScreen extends StatelessWidget {
- final Document document;
+  final Document document;
 
- const DocumentScreen({
-   required this.document,
-   Key? key,
- }) : super(key: key);
+  const DocumentScreen({
+    required this.document,
+    Key? key,
+  }) : super(key: key);
 
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: const Text('Title goes here'),
-     ),
-     body: Column(
-       children: [
-         Center(
-           child: Text('Body goes here'),
-         ),
-       ],
-     ),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    var metadata = document.getMetadata();
+    var title = metadata['title'] as String;
+    var modified = metadata['modified'] as DateTime;
+    var blocks = document.getBlocks(); 
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Text('Last modified: ${formattedModifiedDate(modified)}'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+String formattedModifiedDate(DateTime dateTime) {
+  var today = DateTime.now();
+  var difference = dateTime.difference(today);
+  var days = difference.inDays;
+
+  if (days == 0) {
+    return 'today';
+  } else if (days == 1) {
+    return 'tomorrow';
+  } else if (days == -1) {
+    return 'yesterday';
+  } else if (days > 7) {
+    return '${days ~/ 7} weeks from now';
+  } else if (days < -7) {
+    return '${days.abs() ~/ 7} weeks ago';
+  } else if (days < 0) {
+    return '${days.abs()} days ago';
+  } else {
+    return '$days days from now';
+  }
+}
+
 }
